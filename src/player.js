27e0f7cd2f5761f80c13100ca1dd5d4a080571a0
Dino Tudor <dinotudor@gmail.com>
2019-04-22 17:49:41 +0200

    refactor player JS to ES6

--- src/player.js
+++ src/player.js
@@ -1,6 +1,7 @@
 'use strict';
 
-function Player (canvas) {
+class Player {
+  constructor (canvas) {
   this.lives = 3;
   this.canvas = canvas;
   this.height = 75;
@@ -11,40 +12,42 @@ function Player (canvas) {
   this.speed = 8;
   this.direction = 0;
   this.img = document.getElementById('hero');
-}
+  } 
 
-Player.prototype.draw = function(){
-  //this.ctx.fillStyle = 'white';
-  //this.ctx.fillRect(this.x - this.size/2, this.y-this.size/2, this.size, this.size);
-      //context.drawImage(img,x,y,width,height);
-  this.ctx.drawImage(this.img, this.x - this.width/2, this.y-this.height/2, this.width, this.height);
-  //this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-
-} 
-
-Player.prototype.update = function () {  
-  this.x = this.x + this.direction * this.speed;
-}
-
-Player.prototype.move = function (newDirection) {
-  if (!this.checkLimits(newDirection)) {
-    this.direction = newDirection;
+  draw () {
+    this.ctx.drawImage(this.img, this.x - this.width/2, this.y-this.height/2, this.width, this.height);
   }
+
+  update () {  
+    this.x = this.x + this.direction * this.speed;
+  }
+  
+  move (newDirection) {
+    if (!this.checkLimits(newDirection)) {
+      this.direction = newDirection;
+    }
+  }
+  setLives (){
+    this.lives --;
+  }
+
+  checkCollisionWithEnemy (enemy){
+    const collisionRigth = this.x + this.width/2 > enemy.x - enemy.width/2;
+    const collisionLeft = this.x - this.width/2 < enemy.x + enemy.width/2;
+    const collisionTop = this.y - this.height/2 < enemy.y + enemy.height/2;
+    const collisionBottom = this.y + this.height/2 > enemy.y - enemy.height/2;
+  
+    return collisionRigth && collisionLeft && collisionTop && collisionBottom;
+  }
+
+  checkLimits (direction){
+    return (this.x - this.width/2 < 0 && direction === -1|| this.x + this.width/2 > this.canvas.width && direction === 1)
+  }
+
 }
 
-Player.prototype.setLives = function(){
-  this.lives --;
-}
 
-Player.prototype.checkCollisionWithEnemy = function(enemy){
-  const collisionRigth = this.x + this.width/2 > enemy.x - enemy.width/2;
-  const collisionLeft = this.x - this.width/2 < enemy.x + enemy.width/2;
-  const collisionTop = this.y - this.height/2 < enemy.y + enemy.height/2;
-  const collisionBottom = this.y + this.height/2 > enemy.y - enemy.height/2;
 
-  return collisionRigth && collisionLeft && collisionTop && collisionBottom;
-}
 
-Player.prototype.checkLimits = function(direction){
-  return (this.x - this.width/2 < 0 && direction === -1|| this.x + this.width/2 > this.canvas.width && direction === 1)
-}
+
+
