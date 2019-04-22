21c09e55b38a85e626c951dbcc8ccf7f75e10118
Dino Tudor <dinotudor@gmail.com>
2019-04-22 17:53:27 +0200

    refactor enemy JS to ES6

--- src/enemy.js
+++ src/enemy.js
@@ -1,6 +1,7 @@
 'use strict';
 
-function Enemy (canvas, x) {
+class Enemy  {
+  constructor (canvas, x) {
   this.canvas = canvas;
   this.ctx = this.canvas.getContext('2d');
   this.height = 75;
@@ -10,23 +11,25 @@ function Enemy (canvas, x) {
   this.x = x;
   this.y = 0 + this.height/2;
   this.img = document.getElementById('enemy');
+  }
+  
+  draw (){
+    this.ctx.drawImage(this.img, this.x - this.width/2, this.y - this.width/2, 100, 75);
+  }
+
+  update (gameSpeed){
+    this.y = this.y + this.direction * (this.speed * gameSpeed);
+  }
+  
+  checkHitByBullet (bullet){
+    const collisionRigth = this.x + this.width/2 > bullet.x - bullet.size/2;
+    const collisionLeft = this.x - this.width/2 < bullet.x + bullet.size/2;
+    const collisionTop = this.y - this.height/2 < bullet.y + bullet.size/2;
+    const collisionBottom = this.y + this.height/2 > bullet.y - bullet.size/2;
+  
+    return collisionRigth && collisionLeft && collisionTop && collisionBottom;
+  }
+  
 }
 
-Enemy.prototype.draw = function(){
-  //this.ctx.fillStyle = 'red';
-  //this.ctx.fillRect(this.x - this.size/2, this.y - this.size/2, this.size, this.size)
-  this.ctx.drawImage(this.img, this.x - this.width/2, this.y - this.width/2, 100, 75);
-}
 
-Enemy.prototype.update = function(gameSpeed){
-  this.y = this.y + this.direction * (this.speed * gameSpeed);
-}
-
-Enemy.prototype.checkHitByBullet = function(bullet){
-  const collisionRigth = this.x + this.width/2 > bullet.x - bullet.size/2;
-  const collisionLeft = this.x - this.width/2 < bullet.x + bullet.size/2;
-  const collisionTop = this.y - this.height/2 < bullet.y + bullet.size/2;
-  const collisionBottom = this.y + this.height/2 > bullet.y - bullet.size/2;
-
-  return collisionRigth && collisionLeft && collisionTop && collisionBottom;
-}
